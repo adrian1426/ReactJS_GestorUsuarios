e61195e2935a368b5acd9a47be128a76332a8779
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2021-11-06 18:19:35 -0600

    custom hook useform

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { useForm } from "./hooks/useForm";
+
+const initialState = {
+  nombre: ''
+};
+
 function App() {
+  const [form, handleChange] = useForm(initialState);
+
   return (
-    <div>
-      App js
-    </div>
+    <form>
+      <input
+        name='nombre'
+        placeholder='Nombre'
+        value={form.nombre}
+        onChange={handleChange}
+      />
+    </form>
   );
 }
 
