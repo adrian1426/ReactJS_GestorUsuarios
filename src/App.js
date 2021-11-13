8627939051a2dde48bd528223058b4a5daed01ae
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2021-11-13 12:53:01 -0600

    Card y Container

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import Card from "./components/Card";
+import Container from "./components/Container";
 import Input from "./components/Input";
 import { useForm } from "./hooks/useForm";
 
@@ -10,21 +12,25 @@ function App() {
   const [form, handleChange] = useForm(initialState);
 
   return (
-    <form>
-      <Input
-        name='nombre'
-        value={form.nombre}
-        onChange={handleChange}
-        label='Nombre'
-      />
+    <Container>
+      <Card>
+        <form>
+          <Input
+            name='nombre'
+            value={form.nombre}
+            onChange={handleChange}
+            label='Nombre'
+          />
 
-      <Input
-        name='apellido'
-        value={form.apellido}
-        onChange={handleChange}
-        label='Apellido'
-      />
-    </form>
+          <Input
+            name='apellido'
+            value={form.apellido}
+            onChange={handleChange}
+            label='Apellido'
+          />
+        </form>
+      </Card>
+    </Container>
   );
 }
 
